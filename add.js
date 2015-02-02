dd5be266804d506962a8df29b1d426ea90bb42e6
Jin Dai <jindai1783@gmail.com>
2015-02-02 12:50:52 +0000

    Completed baby step

--- add.js
+++ add.js
@@ -1,1 +1,20 @@
-console.log(process.argv)
+// var input = process.argv;
+
+// input.shift();
+// input.shift();
+
+// var sum = 0;
+
+// input.forEach(function(element) {
+//   sum += Number(element);
+// });
+
+// console.log(sum);
+
+
+var result = 0;
+
+for (i = 2; i < process.argv.length; i++)
+  result += Number(process.argv[i]);
+
+console.log(result);
